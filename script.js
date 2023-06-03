c9d601cca8200a3abae0033e8cfdbbd7fc53d2f3
Filiberto_Castro <fcastrob@miumg.edu.gt>
2023-06-03 16:47:27 -0600

    Metodo para texto animado

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
+
+var divTexto = document.getElementById("texto-animado");
+var textoCompleto = "Conoceréis la verdad y la verdad os hará libres";
+
+var contador = 0;
+var textoMostrado = "";
+
+function mostrarTexto() {
+  if (contador < textoCompleto.length) {
+    var siguienteCaracter = textoCompleto.charAt(contador);
+    
+    textoMostrado += siguienteCaracter;
+    
+    divTexto.innerHTML = textoMostrado;
+    
+    contador++;
+    
+    setTimeout(mostrarTexto, 100); // Puedes ajustar el tiempo según tu preferencia
+  }
+}
+
+mostrarTexto();
